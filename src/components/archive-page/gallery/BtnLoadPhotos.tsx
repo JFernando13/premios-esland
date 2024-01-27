01954e1fdc471034935fa5b15d4b8ea503c1bc7b
JFernando13 <juan_fer-13@hotmail.com>
2024-01-27 13:00:23 -0500

    finish gallery v2

--- src/components/archive-page/gallery/BtnLoadPhotos.tsx
+++ src/components/archive-page/gallery/BtnLoadPhotos.tsx
@@ -1,3 +1,4 @@
+import { useState } from "preact/hooks"
 import "./BtnLoadPhotos.css"
 
 interface Props {
@@ -7,13 +8,48 @@ interface Props {
 
 export function BtnLoadPhotos({ edition, offset }: Props) {
 
+  const [isOpen, setIsOpen] = useState(true)
+
   const loadPhotos = async () => {
     const res = await fetch(`/api/gallery.json?edition=${edition}&offset=${offset}`)
-    const data = await res.json()
+    const { remainingPhotos }: { remainingPhotos: { width: number, height: number }[] } = await res.json()
 
-    console.log(data)
+    const gallery = document.getElementById('gallery')
+
+    const listImg = remainingPhotos.map((img, index) => {
+      const containerImg = document.createElement('div')
+      const firstImg = document.createElement('img')
+      const secondImg = document.createElement('img')
+
+      const srcImg = `/images/editions/${edition + 1}/gallery/thumbnails/img-${+ offset + index + 1}.webp`
+
+      // firstImg
+      firstImg.setAttribute("src", srcImg)
+      firstImg.setAttribute("width", img.width.toString())
+      firstImg.setAttribute("height", img.height.toString())
+      firstImg.classList.add("first-img")
+
+      // secondImg
+      secondImg.setAttribute("src", srcImg)
+      secondImg.setAttribute("width", img.width.toString())
+      secondImg.setAttribute("height", img.height.toString())
+      secondImg.classList.add("second-img")
+
+      // containerImg
+      containerImg.appendChild(firstImg)
+      containerImg.appendChild(secondImg)
+
+      containerImg.classList.add("img-container")
+
+      return containerImg.outerHTML
+    }).join("")
+
+    gallery?.insertAdjacentHTML('beforeend', listImg)
+    setIsOpen(false)
   }
-  return (
-    <button class={'load-photos'} onClick={loadPhotos}>descubrelas todas</button>
-  )
+  return <>
+    {
+      isOpen && <button class={'load-photos'} onClick={loadPhotos}>descubrelas todas</button>
+    }
+  </>
 }
