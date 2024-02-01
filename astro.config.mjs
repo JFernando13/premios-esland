e080e9e6fd206602b0d0e2ecedbdd388a463fe45
JFernando13 <juan_fer-13@hotmail.com>
2024-02-01 15:37:15 -0500

    add astro auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+
+import vercel from '@astrojs/vercel/serverless';
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
-  output: "server"
+  integrations: [tailwind({
+    nesting: true
+  }), preact(), auth({
+    configFile: "auth.config.js",
+  })],
+  output: "server",
+  adapter: vercel(),
 });
