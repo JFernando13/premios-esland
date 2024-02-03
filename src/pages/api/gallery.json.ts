f286ef98069efff91399033ed1b8b123a250f7b5
JFernando13 <juan_fer-13@hotmail.com>
2024-02-03 16:09:52 -0500

    refactor gallery api

--- src/pages/api/gallery.json.ts
+++ src/pages/api/gallery.json.ts
@@ -1,14 +1,11 @@
 import type { APIRoute } from "astro";
 import gallery from "~/data/gallery.json";
 
-export const GET: APIRoute = async ({ url, request }) => {
-  console.log(request.url);
+export const GET: APIRoute = async ({ url }) => {
   const searchParams = url.searchParams;
 
   const edition = Number(searchParams.get("edition") ?? "1");
   const offset = Number(searchParams.get("offset") ?? "0");
-
-  console.log(url.searchParams);
 
   const remainingPhotos = gallery[edition].slice(offset);
 
